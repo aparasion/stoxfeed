ca65c1fc1359bfca48d70610078244b839d5bf3f
Adam Parasion <101869636+aparasion@users.noreply.github.com>
2026-03-06 18:17:13 +0100

    Update generate_gists.py

--- scripts/generate_gists.py
+++ scripts/generate_gists.py
@@ -25,7 +25,7 @@ FEEDS = [
     "https://www.atanet.org/news/industry-news/feed/",
     "https://elia-association.org/news/feed/",
     "https://multilingual.com/news/feed",
-    "https://aparasion.github.io/rss-generator/rss/XTM%20Blog.xml",
+    "https://aparasion.github.io/rss-generator/rss/XTM-Blog.xml",
 ]
 
 SEEN_FILE = "seen.json"
